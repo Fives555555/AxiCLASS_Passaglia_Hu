4862bb67c5e5ed2782880d3565fb74fa1571b08b
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2024-05-10 13:01:36 +0100

    Replace m with mH0

--- source/background.c
+++ source/background.c
@@ -550,50 +550,6 @@ int background_functions(
       // D_sw = -(3./2.)*pvecback[pba->index_bg_H] + pvecback[pba->index_bg_H_prime]/pvecback[pba->index_bg_H]; //NEW
 
 
-      // With m -> m*H_0
-
-      D_sw = -3*pvecback_B[pba->index_bg_H]/2 + pvecback_B[pba->index_bg_H_prime]/pvecback_B[pba->index_bg_H]; 
-
-
-      factor = 1/(4*pow(pba->m_scf,2)*pow(pba->H0,2) + pow(D_sw,2) + 3*D_sw*pvecback_B[pba->index_bg_H]);
-
-
-      phi_c = phi;
-
-
-      phi_prime_c = factor*(-3*a*pvecback_B[pba->index_bg_H]*(2*pow(pba->m_scf,2)*pow(pba->H0,2)*phi + D_sw*phi_prime/a + 
-          3*pvecback_B[pba->index_bg_H]*phi_prime/a));
-
-
-      phi_s = (phi_prime - phi_prime_c)/(pba->m_scf*pba->H0*a);
-
-
-      phi_prime_s = factor*(3*pvecback_B[pba->index_bg_H]*a*(D_sw*pba->m_scf*pba->H0*phi - 2*pba->m_scf*pba->H0*phi_prime/a));
-
-
-
-      printf("a %e \n", a);
-      printf("phi %e \n", phi);
-      printf("phi_prime %e \n", phi_prime);
-      printf("phi_c %e \n", phi_c);
-      printf("phi_s %e \n", phi_s);
-      printf("phi_c_prime %e \n", phi_prime_c/(pba->m_scf*pba->H0*a));
-      printf("phi_s_prime %e \n", phi_prime_s/(pba->m_scf*pba->H0*a));
-
-
-
-      pvecback_B[pba->index_bi_rho_scf] = (phi_prime*phi_prime/(2*a*a) + V_scf(pba,phi))/3.;
-
-      printf("Original rho %e \n", pvecback_B[pba->index_bi_rho_scf]);
-
-
-      pvecback_B[pba->index_bi_rho_scf] = 0.5*(pow(pba->m_scf,2)*pow(pba->H0,2)*(pow(phi_s,2) + pow(phi_c,2)) + 0.5*(pow(phi_prime_c,2) + 
-          pow(phi_prime_s,2))/pow(a,2) + pba->m_scf*pba->H0*(-phi_c*phi_prime_s + phi_s*phi_prime_c)/a)/3.0;
-
-      printf("New rho %e \n", pvecback_B[pba->index_bi_rho_scf]);
-
-
-
 
 
       // // Original equations - IGNORE
@@ -613,35 +569,8 @@ int background_functions(
     // pvecback_B[pba->a_c]
 
 
-      // // MINE
-      // factor =  6*pow(pvecback[pba->index_bg_H],2)/(9*pow(pvecback[pba->index_bg_H],4) - 
-      //     4*(4*pow(pvecback[pba->index_bg_H],2)*pow(pba->m_scf,2) + pow(pvecback[pba->index_bg_H_prime],2)/pow(a,2)));
 
-
-      // // phi_s = factor*(-4*pvecback[pba->index_bg_H]*pba->m_scf*phi - phi_prime*((3*pow(pvecback[pba->index_bg_H],2)/
-      // //     (2*pba->m_scf*a)) + (8*pba->m_scf/(3*a)) + (2*pow(pvecback[pba->index_bg_H_prime], 2)/(3*pba->m_scf*pow(pvecback[pba->index_bg_H],2)*pow(a,3))) +
-      // //     (2*pvecback[pba->index_bg_H_prime]/(pba->m_scf*pow(a,2)))));
-
-    
-      // phi_prime_c = factor*a*pba->m_scf*(4*pvecback[pba->index_bg_H]*pba->m_scf*phi + (3*pow(pvecback[pba->index_bg_H],2)*phi_prime/(pba->m_scf*a)) + 
-      //     (2*pvecback[pba->index_bg_H_prime]*phi_prime/(pba->m_scf*pow(a,2))));
-
-      
-      // phi_s = phi_prime/a - phi_prime_c/a;
-
-
-      // phi_prime_s = factor*a*pba->m_scf*(3*pow(pvecback[pba->index_bg_H],2)*phi - 2*pvecback[pba->index_bg_H_prime]*phi/a + 
-      //     4*pvecback[pba->index_bg_H]*phi_prime/a);
-
-
-      // // Original equation - MINE
-      // pvecback_B[pba->index_bi_rho_scf] = (0.5*( pow(pba->m_scf,2)*pow(phi_c,2) + pow(pba->m_scf,2)*pow(phi_s,2) + 0.5*pow(phi_prime_c,2)/pow(a,2) + 
-      //     0.5*pow(phi_prime_s,2)/pow(a,2) - pba->m_scf*phi_c*phi_prime_s/a + pba->m_scf*phi_s*phi_prime_c/a))/3.0;
-
-
-
-
-      // // TriggerCLASS
+      // // TriggerCLASS - IGNORE
 
       // factor = 6*pow(pvecback[pba->index_bg_H],2)/(9*pow(pvecback[pba->index_bg_H],4) - 4*(4*pow(pvecback[pba->index_bg_H],2)*pow(pba->m_scf,2) + 
       //     pow(pvecback[pba->index_bg_H_prime],2)/pow(a,2)));
@@ -665,6 +594,94 @@ int background_functions(
       // // TriggerCLASS equation
       // pvecback_B[pba->index_bi_rho_scf] = (0.5 * pow(pba->m_scf, 2) * (phi_c * phi_c + phi_s * phi_s + 0.5 * (phi_prime_c * phi_prime_c + 
       //     phi_prime_s * phi_prime_s) - phi_c * phi_prime_s + phi_s * phi_prime_c))/3.0;
+
+
+
+
+
+
+
+
+
+
+
+
+      // With m -> m*H_0 - Without D substituted
+
+      D_sw = -3*pvecback[pba->index_bg_H]/2 + pvecback[pba->index_bg_H_prime]/(pvecback[pba->index_bg_H]*a); 
+
+
+      factor = 1/(4*pow(pba->m_scf,2)*pow(pba->H0,2) + pow(D_sw,2) + 3*D_sw*pvecback[pba->index_bg_H]);
+
+
+      phi_c = phi;
+
+
+      phi_prime_c = factor*(-3*a*pvecback[pba->index_bg_H]*(2*pow(pba->m_scf,2)*pow(pba->H0,2)*phi + D_sw*phi_prime/a + 
+          3*pvecback[pba->index_bg_H]*phi_prime/a));
+
+
+      phi_s = (phi_prime - phi_prime_c)/(pba->m_scf*pba->H0*a);
+
+
+      phi_prime_s = factor*(3*pvecback[pba->index_bg_H]*a*(D_sw*pba->m_scf*pba->H0*phi - 2*pba->m_scf*pba->H0*phi_prime/a));
+
+
+      pvecback_B[pba->index_bi_rho_scf] = 0.5*(pow(pba->m_scf,2)*pow(pba->H0,2)*(pow(phi_s,2) + pow(phi_c,2)) + 0.5*(pow(phi_prime_c,2) + 
+          pow(phi_prime_s,2))/pow(a,2) + pba->m_scf*pba->H0*(-phi_c*phi_prime_s + phi_s*phi_prime_c)/a)/3.0;
+
+
+
+
+
+
+
+      // With m -> m*H_0 - With D substituted
+      factor =  6*pow(pvecback[pba->index_bg_H],2)/(9*pow(pvecback[pba->index_bg_H],4) - 
+          4*(4*pow(pvecback[pba->index_bg_H],2)*pow(pba->m_scf*pba->H0,2) + pow(pvecback[pba->index_bg_H_prime],2)/pow(a,2)));
+
+
+      phi_c = phi;
+
+
+    
+      phi_prime_c = factor*a*pba->m_scf*pba->H0*(4*pvecback[pba->index_bg_H]*pba->m_scf*pba->H0*phi + 
+          (3*pow(pvecback[pba->index_bg_H],2)*phi_prime/(pba->m_scf*pba->H0*a)) + 
+          (2*pvecback[pba->index_bg_H_prime]*phi_prime/(pba->m_scf*pba->H0*pow(a,2))));
+
+      
+      phi_s = (phi_prime - phi_prime_c)/(a*pba->m_scf*pba->H0);
+
+
+      phi_prime_s = factor*a*pba->m_scf*pba->H0*(3*pow(pvecback[pba->index_bg_H],2)*phi - 2*pvecback[pba->index_bg_H_prime]*phi/a + 
+          4*pvecback[pba->index_bg_H]*phi_prime/a);
+
+
+      // 
+      pvecback_B[pba->index_bi_rho_scf] = (0.5*(pow(pba->m_scf*pba->H0,2)*(pow(phi_c,2) + pow(phi_s,2)) + 
+          (pow(phi_prime_c,2) + pow(phi_prime_s,2))/(2*pow(a,2)) + pba->m_scf*pba->H0*(-phi_c*phi_prime_s + phi_s*phi_prime_c)/a))/3.0;
+
+
+
+
+      printf("a %e \n", a);
+      printf("phi %e \n", phi);
+      printf("phi_prime %e \n", phi_prime);
+      printf("phi_c %e \n", phi_c);
+      printf("phi_s %e \n", phi_s);
+      printf("phi_c_prime %e \n", phi_prime_c/(pba->m_scf*pba->H0*a));
+      printf("phi_s_prime %e \n", phi_prime_s/(pba->m_scf*pba->H0*a));
+
+
+
+      printf("New rho %e \n", pvecback_B[pba->index_bi_rho_scf]);
+
+
+
+      pvecback_B[pba->index_bi_rho_scf] = (phi_prime*phi_prime/(2*a*a) + V_scf(pba,phi))/3.;
+
+      printf("Original rho %e \n", pvecback_B[pba->index_bi_rho_scf]);
+
 
 
 
