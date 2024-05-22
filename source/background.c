cb32bae70cbb99acfbb80938798d8cc87d0a7abd
Adam Moss <3430494+adammoss@users.noreply.github.com>
2024-05-22 09:24:02 +0100

    wip

--- source/background.c
+++ source/background.c
@@ -411,6 +411,26 @@ int background_functions(
      Note: The scalar field contribution must be added in the end, as an exception!*/
   double dp_dloga;
 
+    /**ADDITIONAL LOCAL VARIABLES*/
+  double D_sw;
+  double phi_c;
+  double phi_s;
+  double phi_prime_c;
+  double phi_prime_s;
+  double factor;
+  double H;
+  double Hprime;
+  /**INITIALISE ADDITIONAL LOCAL VARIABLES*/
+  D_sw = 0.;
+  phi_c = 0.;
+  phi_s = 0.;
+  phi_prime_c = 0.;
+  phi_prime_s = 0.;
+  factor = 0.;
+  H = 0.;
+  Hprime = 0.;
+
+
   /** - initialize local variables */
   rho_tot = 0.;
   p_tot = 0.;
@@ -492,6 +512,19 @@ int background_functions(
     pvecback[pba->index_bg_ddV_scf] = ddV_scf(pba,phi); // ddV_scf(pba,phi); //potential'' as function of phi
     pvecback[pba->index_bg_rho_scf] = (phi_prime*phi_prime/(2*a*a) + V_scf(pba,phi))/3.; // energy of the scalar field. The field units are set automatically by setting the initial conditions
     pvecback[pba->index_bg_p_scf] = (phi_prime*phi_prime/(2*a*a) - V_scf(pba,phi))/3.; // pressure of the scalar field
+
+    H = sqrt(rho_tot-pba->K/a/a);
+    Hprime = - (3./2.) * (rho_tot + p_tot) * a + pba->K/a;
+    factor =  6*pow(H,2)/(9*pow(H,4) - 4*(4*pow(H,2)*pow(pba->m_scf*pba->H0,2) + pow(Hprime,2)/pow(a,2)));
+    phi_c = phi;
+    phi_prime_c = factor*a*pba->m_scf*pba->H0*(4*H*pba->m_scf*pba->H0*phi +
+          (3*pow(H,2)*phi_prime/(pba->m_scf*pba->H0*a)) +
+          (2*Hprime*phi_prime/(pba->m_scf*pba->H0*pow(a,2))));
+    phi_s = (phi_prime - phi_prime_c)/(a*pba->m_scf*pba->H0);
+    phi_prime_s = factor*a*pba->m_scf*pba->H0*(3*pow(H,2)*phi - 2*Hprime*phi/a +
+          4*H*phi_prime/a);
+    pvecback[pba->index_bg_rho_scf_aux] = (0.5*(pow(pba->m_scf*pba->H0,2)*(pow(phi_c,2) + pow(phi_s,2)) +
+          (pow(phi_prime_c,2) + pow(phi_prime_s,2))/(2*pow(a,2)) + pba->m_scf*pba->H0*(-phi_c*phi_prime_s + phi_s*phi_prime_c)/a))/3.0;
 
     pvecback[pba->index_bg_w_scf] =pvecback[pba->index_bg_p_scf]/pvecback[pba->index_bg_rho_scf]; // e.o.s of the scalar field, only used for outputs
     pvecback_B[pba->index_bi_rho_scf] = pvecback[pba->index_bg_rho_scf];
@@ -3273,6 +3306,7 @@ int background_derivs(
     // if(pba->background_verbose > 11) printf("Evolving scalar field using KG equation. phi %e phi prime %e \n", y[pba->index_bi_phi_scf],dy[pba->index_bi_phi_scf]  );
     }
     else if(pba->scf_kg_eq == _FALSE_) {
+    printf("Evolution rho %e %e %e \n", a, y[pba->index_bi_rho_scf], pba->w_scf);
     dy[pba->index_bi_rho_scf] = -3.*y[pba->index_bi_rho_scf]*(1+pba->w_scf);
     dy[pba->index_bi_phi_scf] = 0;
     dy[pba->index_bi_phi_prime_scf] = 0;
