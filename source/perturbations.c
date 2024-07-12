fb7797dbcb9e6d8b8ef685c2e17a25d971a0c933
Fives555555 <108680435+Fives555555@users.noreply.github.com>
2024-07-12 17:15:46 +0100

    Function format

--- source/perturbations.c
+++ source/perturbations.c
@@ -7231,6 +7231,98 @@ int perturbations_einstein(
 
 }
 
+// Might need to move this above the previous section
+void PH_delta(
+                      struct background * pba,
+                      struct perturbations * ppt,
+                      struct perturbations_workspace * ppw,
+                      double k,
+                      double aux_deltas[]
+                      )
+  {
+  // PH and local variables
+  double a = 0.;
+  double H = 0.;
+  double Hprime = 0.;
+  double factor_PH = 0.;
+  double phi_c = 0.;
+  double phi_s = 0.;
+  double phi_prime_c = 0.;
+  double phi_prime_s = 0.;
+  double delta_phi_c = 0.;
+  double delta_phi_s = 0.;
+  double delta_phi_prime_c = 0.;
+  double delta_phi_prime_s = 0.;
+  double hL_prime = 0.;
+  double delta_rho_scf = 0.;
+  double delta_p_scf = 0.;
+
+  // calculate H, Hprime, auxiliaries to go into delta
+  hL_prime = ppw->pvecmetric[ppw->index_mt_h_prime];
+  a = ppw->pvecback[pba->index_bg_a];
+
+  H = sqrt((ppw->pvecback[pba->index_bg_rho_tot] + ppw->pvecback[pba->index_bg_rho_scf])-pba->K/a/a);
+
+  Hprime = - (3./2.) * ((ppw->pvecback[pba->index_bg_rho_tot] + ppw->pvecback[pba->index_bg_rho_scf]) + 
+          (ppw->pvecback[pba->index_bg_p_tot] + ppw->pvecback[pba->index_bg_p_scf])) * a + pba->K/a;
+
+  factor_PH = 6*pow(H,2)/(9*pow(H,4) - 4*(4*pow(H,2)*pow(pba->m_scf*pba->H0,2) + pow(Hprime,2)/pow(a,2)));
+
+  phi_c = ppw->pvecback[pba->index_bi_phi_scf];
+
+  phi_prime_c = factor_PH*a*pba->m_scf*pba->H0*(4*H*pba->m_scf*pba->H0*ppw->pvecback[pba->index_bi_phi_scf] +
+              (3*pow(H,2)*ppw->pvecback[pba->index_bi_phi_prime_scf]/(pba->m_scf*pba->H0*a)) +
+              (2*Hprime*ppw->pvecback[pba->index_bi_phi_prime_scf]/(pba->m_scf*pba->H0*pow(a,2))));
+
+  phi_s = (ppw->pvecback[pba->index_bi_phi_prime_scf] - phi_prime_c)/(a*pba->m_scf*pba->H0);
+
+  phi_prime_s = factor_PH*a*pba->m_scf*pba->H0*(3*pow(H,2)*ppw->pvecback[pba->index_bi_phi_scf] - 2*Hprime*ppw->pvecback[pba->index_bi_phi_scf]/a + 
+              4*H*ppw->pvecback[pba->index_bi_phi_prime_scf]/a);
+
+  delta_phi_c = ppt->index_tp_delta_phi_scf;
+
+  delta_phi_prime_c = (H*(-8*ppt->index_tp_delta_phi_prime_scf*H*pow(k,2) + 4*ppt->index_tp_delta_phi_scf*Hprime*pow(k,2) + 
+                    24*pow(a,3)*ppt->index_tp_delta_phi_scf*pow(H,2)*pow(pba->m_scf*pba->H0,2) + 2*hL_prime*Hprime*phi_prime_c + 
+                    pow(a,2)*H*(18*ppt->index_tp_delta_phi_prime_scf*pow(H,2) + hL_prime*pba->m_scf*pba->H0*(4*pba->m_scf*pba->H0*phi_c + 3*H*phi_s)) + 
+                    a*(12*ppt->index_tp_delta_phi_prime_scf*H*Hprime + 6*ppt->index_tp_delta_phi_scf*pow(H,2)*pow(k,2) + 3*pow(H,2)*hL_prime*phi_prime_c + 
+                    2*hL_prime*Hprime*pba->m_scf*pba->H0*phi_s - 4*H*hL_prime*pba->m_scf*pba->H0*phi_prime_s)))/(-4*a*(pow(Hprime,2) + 2*pow(H,2)*pow(k,2)) + 
+                    pow(a,3)*(9*pow(H,4) - 16*pow(H,2)*pow(pba->m_scf*pba->H0,2)));
+
+  delta_phi_s = (ppt->index_tp_delta_phi_prime_scf - delta_phi_prime_c)/(a*pba->m_scf*pba->H0);
+
+  delta_phi_prime_s = -((H*(18*pow(a,4)*ppt->index_tp_delta_phi_scf*pow(H,3)*pow(pba->m_scf*pba->H0,2) + 2*pow(k,2)*(2*ppt->index_tp_delta_phi_prime_scf*Hprime + 
+                    2*ppt->index_tp_delta_phi_scf*H*pow(k,2) + H*hL_prime*phi_prime_c) + pow(a,3)*H*pow(pba->m_scf*pba->H0,2)*(24*ppt->index_tp_delta_phi_prime_scf*H - 
+                    12*ppt->index_tp_delta_phi_scf*Hprime + 3*H*hL_prime*phi_c + 4*hL_prime*pba->m_scf*pba->H0*phi_s) + 2*a*(H*pow(k,2)*(3*ppt->index_tp_delta_phi_prime_scf*H + 
+                    hL_prime*pba->m_scf*pba->H0*phi_s) + hL_prime*Hprime*pba->m_scf*pba->H0*phi_prime_s) + 
+                    pow(a,2)*pba->m_scf*pba->H0*(8*ppt->index_tp_delta_phi_scf*H*pow(k,2)*pba->m_scf*pba->H0 - 2*hL_prime*Hprime*pba->m_scf*pba->H0*phi_c + 
+                    H*hL_prime*(4*pba->m_scf*pba->H0*phi_prime_c - 3*H*phi_prime_s))))/(pow(a,2)*pba->m_scf*pba->H0*(4*pow(Hprime,2) + pow(H,2)*(8*pow(k,2) + 
+                    pow(a,2)*(-9*pow(H,2) + 16*pow(pba->m_scf*pba->H0,2))))));
+
+  delta_rho_scf = (pba->m_scf*pba->H0*a*phi_s*delta_phi_prime_c - pba->m_scf*pba->H0*a*phi_c*delta_phi_prime_s + phi_prime_c*delta_phi_prime_c + phi_prime_s*delta_phi_prime_s + 
+                delta_phi_s*(2*pow(pba->m_scf*pba->H0*a,2)*phi_s + pba->m_scf*pba->H0*a*phi_prime_c) + delta_phi_c*(2*pow(pba->m_scf*pba->H0*a,2)*phi_c - 
+                pba->m_scf*pba->H0*a*phi_prime_s))/(2*pow(a,2));
+
+  delta_p_scf = delta_rho_scf - pow(pba->m_scf*pba->H0,2)*(phi_c*delta_phi_c + phi_s*delta_phi_s);
+
+  // return delta_rho_scf/ppw->pvecback[pba->index_bg_rho_scf]; // NEED TO ALSO RETURN DELTA_P?????
+
+  // y[ppw->pv->index_pt_delta_scf] = delta_rho_scf/ppw->pvecback[pba->index_bg_rho_scf];
+  aux_deltas[0] = delta_rho_scf;
+  aux_deltas[1] = delta_p_scf; // NEED TO DEFINE ARRAY LOCALLY BEFORE PASSING INTO THIS FUNCTION
+
+  // COULD RETURN ALL AUXILIARIES?
+  // COULD ALSO DO SAME WITH H AND HPRIME?
+
+  }
+
+
+
+
+
+
+
+
+
 int perturbations_total_stress_energy(
                                       struct precision * ppr,
                                       struct background * pba,
@@ -7284,6 +7376,7 @@ int perturbations_total_stress_energy(
   double Gamma_fld, S, S_prime, theta_t, theta_t_prime, rho_plus_p_theta_fld_prime, rho_plus_p_theta_scf,cs2_scf,ca2_scf,a_over_ac;
   double delta_p_b_over_rho_b;
   // PH and local variables
+  double aux_deltas[2]; // 0=delta_rho, 1=delta_p // CAN INCLUDE MORE AND MAKE LARGER
   double H = 0.;
   double Hprime = 0.;
   double factor_PH = 0.;
@@ -7607,10 +7700,13 @@ int perturbations_total_stress_energy(
                 delta_p_scf = delta_rho_scf - pow(pba->m_scf*pba->H0,2)*(phi_c*delta_phi_c + phi_s*delta_phi_s);
 
 
-
+                
                 y[ppw->pv->index_pt_delta_scf] = delta_rho_scf/ppw->pvecback[pba->index_bg_rho_scf];
 
 
+                // Alternate using the function
+                PH_delta(pba, ppt, ppw, k, aux_deltas);
+                y[ppw->pv->index_pt_delta_scf] = aux_deltas[0]/ppw->pvecback[pba->index_bg_rho_scf];
 
                 // rho_aux = (0.5*(pow(pba->m_scf*pba->H0,2)*(pow(phi_c,2) + pow(phi_s,2)) +
                 //     (pow(phi_prime_c,2) + pow(phi_prime_s,2))/(2*pow(a,2)) + pba->m_scf*pba->H0*(-phi_c*phi_prime_s + phi_s*phi_prime_c)/a))/3.0;
@@ -7619,9 +7715,6 @@ int perturbations_total_stress_energy(
                 //     pvecback[pba->index_bg_rho_scf] = weight * pvecback[pba->index_bg_rho_scf] + (1 - weight) * rho_aux;
                 //     pvecback[pba->index_bg_p_scf] = pvecback[pba->index_bg_w_scf] * pvecback[pba->index_bg_rho_scf];
                 // }
-
-
-
                 
               }
 
